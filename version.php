a952bbc824eb93a4a28d671b2bef7c7582338291
gthomas2 <gthomas@moodlerooms.com>
2016-07-19 12:38:23 +0100

    INT-9961,INT-10043: Update redis cache store version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version    = 2016012600;
-$plugin->requires   = 2015051103;
+$plugin->requires   = 2015111604; // Requires this Moodle version (3.0.4).
 $plugin->maturity   = MATURITY_STABLE;
 $plugin->component  = 'cachestore_redis';
-$plugin->release    = '2.9.3 (Build: 20160204)';
+$plugin->release    = '3.0.4 (Build: 20160509)';
